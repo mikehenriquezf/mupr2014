cf7794c0943982dbb6e7de76b08e11255c46b72e
Mike Henriquez <mike.henriquezf@gmail.com>
2013-08-30 18:08:44 -0400

    First Commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function (){
+
+	// Slider
+	if ($('#slider-area').length) {
+	$('#slider-area')
+	.after('<div id="nav">') 
+	.cycle({ 
+		fx:     'fade', 
+		speed:  1000,
+		pause: 1, 
+		timeout: 3000,
+		next:   '#next',
+		prev:   '#prev',
+		pager: '#nav' 
+	});
+	}
+
+
+	// $(window).scroll(function (){
+	// 	if($('.sticky').css('width') > '1000px'){
+	// 		$('.title-area .name').removeClass('esconde');
+	// 	}else{
+	// 		$('.title-area .name').addClass('esconde');
+	// 	}
+	// });
+		
+});
