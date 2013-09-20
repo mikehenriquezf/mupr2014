85f20f5989e93d4e476b7d0b74d834419c537e90
Mike Henriquez <mike.henriquezf@gmail.com>
2013-09-20 13:19:35 -0400

    Votaciones functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function (){
 
+	
+
 	// Slider
 	if ($('#slider-area').length) {
 		$('#slider-area').after('<div id="nav">') .cycle({ 
@@ -24,7 +26,11 @@ $(document).ready(function (){
 
     $('.vote_candidata_thumb').on('click', function (){
     	var pueblo = $(this).children('.pueblo').val();
-    	console.log(pueblo);
+    	
+    	$('.registro_votar h2').html('Reg&iacute;strate antes de votar');
+		$('.registro_votar p').html('Para realizar tu voto necesitas autenticarte con Facebook');
+		$('.fb_connect').show();
+
     	$('.overlay').css('opacity', 0);
     	$('.registro_votar').css('opacity', 0);
     	
@@ -40,16 +46,93 @@ $(document).ready(function (){
 
     });
 
+    $('.registro_votar img').on('click', function(){
+    	FB.login(function(response) {
+	        if (response.authResponse) {
+	            console.log('Authenticated!');
+	            checkUser();
+
+	        } else {
+	            console.log('User cancelled login or did not fully authorize.');
+	        }
+	    });
+
+
+    });
+
+	function checkUser() {
+
+		$('.fb_connect').hide();
+		$('.loading').show();
+
+		FB.api('/me', function(response) {
+			var fbid, name, email, gender, hometown, location;
+
+			fbid = response.id; //fbid
+			name = response.name; //n
+			email = response.email; //e
+			gender = response.gender; //g
+			hometown = response.hometown.name; //ht
+			location = response.location.name; //loc
+
+			$.ajax({
+				type: "GET",
+				url: "db_handler.php",
+				data: "fbid="+fbid+"&n="+name+"&e="+email+"&g="+gender+"&ht="+hometown+"&loc="+location,
+				success: function(status){
+					// console.log("fbid="+fbid+"&n="+name+"&e="+email+"&g="+gender+"&ht="+hometown+"&loc="+location);
+					if(status == 1){
+						document.cookie= "vote_status=voted";
+						$('.registro_votar').fadeTo('fast', 0, function(){
+							$('.overlay').fadeTo('slow', 0, function (){
+								$('.registro_votar').hide();
+								$('.overlay').hide();
+
+								// Show results
+								$('.textLight').html('Resultado de votaciones').css({'margin-left':'290px'});
+								$('#vote_candidatas').fadeTo('slow', 0, function(){
+									$('#vote_candidatas').hide();
+									$('#results_candidatas').show().css('opacity', 0);
+									$('#results_candidatas').fadeTo('slow', 1);	
+								});
+								
+							});
+						});
+					}else{
+						document.cookie= "vote_status=voted";
+
+						$('.loading').fadeTo('slow', 0, function(){
+							$('.loading').hide();
+						});
+						$('.registro_votar h2').html('Lo sentimos...');
+						$('.registro_votar p').html('Ya usted excedió la cantidad de votos permitidos.');
+					}	
+				}
+			});
+		});
+	}
+
+
     $('.registro_votar a').on('click', function(){
-    	
+    	voted_status = getCookie("vote_status");
+
+    	if(voted_status == 'voted'){
+    		$('.textLight').html('Resultado de votaciones').css({'margin-left':'290px'});
+    		$('#vote_candidatas').hide();
+    		$('#results_candidatas').show();
+    	}
+
+    	// CERRAR MODAL
 		$('.registro_votar').fadeTo('fast', 0, function(){
 			$('.overlay').fadeTo('slow', 0, function (){
 				$('.registro_votar').hide();
 				$('.overlay').hide();
 			});
-
 		});
     });
+
+    
+
 
     $(window).resize(function(){
     	var dialogTop = ($(window).height() - $('.registro_votar').height() ) / 2 + $(window).scrollTop();
@@ -81,3 +164,7 @@ $(document).ready(function (){
 	});
 		
 });
+
+
+
+
